ae4f98453660a371faad7d18038dcd85a653ed23
Mikhail <46933536+MesseMMP@users.noreply.github.com>
2023-12-02 16:10:16 +0300

    Delete firstKotlinProject directory

--- firstKotlinProject/src/main/kotlin/Echo.kt
+++
@@ -1,9 +0,0 @@
-fun main(args: Array<String>) {
-    val inputString: String = readln()
-    val arrayWords: List<String> = inputString.split(" ")
-    val lastWord: String = arrayWords[arrayWords.lastIndex]
-    println(inputString)
-    repeat(3){
-        println(lastWord)
-    }
-}
--- firstKotlinProject/src/main/kotlin/RemoveDuplicates.kt
+++
@@ -1,9 +0,0 @@
-fun main(args: Array<String>) {
-    var count = 0;
-    for (i in 0..<args.lastIndex){
-        if (args[i] == args[i+1]){
-            ++count
-        }
-    }
-    println(args.size - count)
-}
