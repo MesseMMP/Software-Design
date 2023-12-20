76ad40973566fdb6a6691e4005922679c5100f54
Mikhail <46933536+MesseMMP@users.noreply.github.com>
2023-12-20 15:32:55 +0300

    Delete КПО2 directory

--- КПО2/src/main/kotlin/Customer.kt
+++
@@ -1,9 +0,0 @@
-class Customer(var login: String, private var password: String) {
-    init {
-        password = password.hashCode().toString()
-    }
-
-    override fun toString(): String {
-        return "Login: $login, Password: $password"
-    }
-}
--- КПО2/src/main/kotlin/Product.kt
+++
@@ -1,5 +0,0 @@
-class Product(val id: Int, val name: String, val price: Int, var number : Int) {
-    override fun toString(): String {
-        return "Name = $name, Price = $price, Number: $number"
-    }
-}
